ea2324fd8faacd503b8b965da9788448d90a823f
pedrohp-machado <pedro.machado@estudante.ufscar.br>
2025-07-17 15:18:22 -0300

    Correção da altura do voo

--- src/objetos/ladybugAnimation.ts
+++ src/objetos/ladybugAnimation.ts
@@ -6,13 +6,17 @@ export function createLadybugAnimation() {
     const flyHeightAmplitude = 1;
     const flySpeed = 0.02;
 
+    const minHeight = 1.5;      
+
     return function animateLadybug(ladybug: THREE.Group) {
         flyAngle += flySpeed;
 
         ladybug.position.x = flyRadius * Math.cos(flyAngle);
         ladybug.position.z = flyRadius * Math.sin(flyAngle);
-        ladybug.position.y = 1 + Math.sin(flyAngle * 3) * flyHeightAmplitude;
 
+        const verticalOscillation = (Math.sin(flyAngle * 3) + 1) / 2;
+        ladybug.position.y = minHeight + verticalOscillation * flyHeightAmplitude;
+        
         ladybug.rotation.y = -flyAngle;
     };
 }
